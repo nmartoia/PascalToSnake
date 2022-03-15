f6a1094e815d108f4231817329dc2c065a6b71a5
nmartoia <nmartoia@edenschool.fr>
2022-03-15 16:28:51 +0100

    pascal2snake fini

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 export const pascalToSnake = (entry) => {
     if(parseInt(entry)){
-       return entry; 
+       return entry.toString(); 
     }
     let result = "";
-
+    const caract = ["0","1","2","3","4","5","6","7","8","9","-","_"];
     for (let index = 0; index < entry.length; index++) {
         const element = entry[index];
-        if(element.toUpperCase() === element){
+        if(caract.includes(element)){
+            result += element;
+        } else if (element.toUpperCase() === element){
             if(index === 0){
-                result += element.toLowerCase()
+                result += element.toLowerCase();
             } else {
-                result += "_" + element.toLowerCase()
+                result += "_" + element.toLowerCase();
             }
         } else {
             result += element;
         }
     }
-    
     return result;
 }
-
